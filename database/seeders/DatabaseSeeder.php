1bff45429d137d3e367f3f7a4f990330b9ee3292
rosaary003 <macaaabrew@gmail.com>
2026-07-12 19:18:57 -0700

    fix seeder produk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Product::firstOrCreate(
+            ['name' => 'Super Mario Bros. Wonder'],
+            [
+                'category' => 'Adventure',
+                'description' => 'Petualangan seru Mario di Flower Kingdom',
+                'price' => 650000,
+                'stock' => 10,
+                'image' => 'mario-wonder.jpg',
+            ]
+        );
     }
 }
